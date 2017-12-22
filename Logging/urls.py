d66398db5424fbaf688ff895c6cdab51c9dca3b8
joshuasellers <joshuasellersd@gmail.com>
2017-12-21 23:36:30 -0500

    Delete urls.py

--- Logging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-    url(r'^$', views.logs, name='log_index'),
-    url(r'^(user|type)$', views.logs_sort, name='log_sorted'),
-
-]
