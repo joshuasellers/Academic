a848ad0942445a3d795a7ebfb1e26323f91b3781
joshuasellers <joshuasellersd@gmail.com>
2017-12-21 23:41:37 -0500

    Delete admin.py

--- Records/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Prescription, TestResults
-
-# Register your models here.
-admin.site.register(Prescription)
-admin.site.register(TestResults)
-
